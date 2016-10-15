63f6e3d0b17f0e5e1b06b0656d7707bc4279fd72
kyorohiro <kyorohiro@gmail.com>
2016-10-16 02:47:00 +0900

    rename

--- lib/core/parser/bytesreader.dart
+++ lib/core/parser/bytesreader.dart
@@ -1,53 +1,27 @@
 part of core;
 
 class BytesReader extends Reader {
-  int _max = 1024;
-  BufferImpl _buffer8;
+  int _bufferSize = 1024;
+  BufferImpl _bufferObj;
   int _length = 0;
 
-  BufferImpl get rawbuffer8 => _buffer8;
+  BufferImpl get rawbuffer8 => _bufferObj;
   List<GetByteFutureInfo> mGetByteFutreList = new List();
 
-  int get clearedBuffer => _buffer8.clearedBuffer;
+  int get clearedBuffer => _bufferObj.clearedBuffer;
 
   bool logon = false;
   BytesReader({bufferSize: 1024}) {
     this.logon = logon;
-    _max = bufferSize;
-    _buffer8 = new BufferImpl(_max); //new data.Uint8List(_max);
+    _bufferSize = bufferSize;
+    _bufferObj = new BufferImpl(_bufferSize); //new data.Uint8List(_max);
   }
 
-  BytesReader.fromList(List<int> buffer, [isFin = false]) {
-    _buffer8 = new BufferImpl.fromList(buffer);
+  BytesReader.fromList(List<int> buffer, [isImmutable = false]) {
+    _bufferObj = new BufferImpl.fromList(buffer);
     _length = buffer.length;
-    if (isFin == true) {
+    if (isImmutable == true) {
       completeDataAppending();
-    }
-  }
-
-  bool _updateGetInfo(GetByteFutureInfo info) {
-    if (this.immutable == true || info.index + info.completerResultLength - 1 < _length) {
-      info.completer.complete(info.index);
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  void _updateGetInfos() {
-    var removeList = null;
-    for (GetByteFutureInfo f in mGetByteFutreList) {
-      if (true == _updateGetInfo(f)) {
-        if (removeList == null) {
-          removeList = [];
-        }
-        removeList.add(f);
-      }
-    }
-    if (removeList != null) {
-      for (GetByteFutureInfo f in removeList) {
-        mGetByteFutreList.remove(f);
-      }
     }
   }
 
@@ -76,21 +50,21 @@ class BytesReader extends Reader {
       out = new data.Uint8List(len >= 0 ? len : 0);
     }
     for (int i = 0; i < len; i++) {
-      out[i] = _buffer8[index + i];
+      out[i] = _bufferObj[index + i];
     }
     return out;
   }
 
-  int operator [](int index) => 0xFF & _buffer8[index];
+  int operator [](int index) => 0xFF & _bufferObj[index];
 
-  int get(int index) => 0xFF & _buffer8[index];
+  int get(int index) => 0xFF & _bufferObj[index];
 
   void clear() {
     _length = 0;
   }
 
   void clearInnerBuffer(int len, {reuse: true}) {
-    _buffer8.clearBuffer(len, reuse: reuse);
+    _bufferObj.clearBuffer(len, reuse: reuse);
   }
 
   int get currentSize => _length;
@@ -104,29 +78,30 @@ class BytesReader extends Reader {
   }
 
   void update(int plusLength) {
-    if (_length + plusLength < _max) {
+    if (_length + plusLength < _bufferSize) {
       return;
     } else {
-      int nextMax = _length + plusLength + (_max - _buffer8.clearedBuffer);
-      _buffer8.expandBuffer(nextMax);
-      _max = nextMax;
+      int nextMax = _length + plusLength + (_bufferSize - _bufferObj.clearedBuffer);
+      _bufferObj.expandBuffer(nextMax);
+      _bufferSize = nextMax;
     }
   }
 
-  void appendByte(int v) {
+  bool appendByte(int v) {
     if (immutable) {
-      return;
+      return false;
     }
     update(1);
-    _buffer8[_length] = v;
+    _bufferObj[_length] = v;
     _length += 1;
 
     _updateGetInfos();
+    return true;
   }
 
-  void appendIntList(List<int> buffer, [int index = 0, int length = -1]) {
+  bool appendIntList(List<int> buffer, [int index = 0, int length = -1]) {
     if (immutable) {
-      return;
+      return false;
     }
     if (length < 0) {
       length = buffer.length;
@@ -134,19 +109,48 @@ class BytesReader extends Reader {
     update(length);
 
     for (int i = 0; i < length; i++) {
-      _buffer8[_length + i] = buffer[index + i];
+      _bufferObj[_length + i] = buffer[index + i];
     }
     _length += length;
     _updateGetInfos();
+    return true;
   }
 
-  void appendString(String text) => appendIntList(convert.UTF8.encode(text));
+  bool appendString(String text) => appendIntList(convert.UTF8.encode(text));
 
-  List toList() => _buffer8.sublist(0, _length);
+  List toList() => _bufferObj.sublist(0, _length);
 
   data.Uint8List toUint8List() => new data.Uint8List.fromList(toList());
 
   String toText() => convert.UTF8.decode(toList());
+  //
+  //
+  //
+  bool _updateGetInfo(GetByteFutureInfo info) {
+    if (this.immutable == true || info.index + info.completerResultLength - 1 < _length) {
+      info.completer.complete(info.index);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void _updateGetInfos() {
+    var removeList = null;
+    for (GetByteFutureInfo f in mGetByteFutreList) {
+      if (true == _updateGetInfo(f)) {
+        if (removeList == null) {
+          removeList = [];
+        }
+        removeList.add(f);
+      }
+    }
+    if (removeList != null) {
+      for (GetByteFutureInfo f in removeList) {
+        mGetByteFutreList.remove(f);
+      }
+    }
+  }
 }
 
 class GetByteFutureInfo {
